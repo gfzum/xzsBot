9d3b2b8b818078ef3950c85b0e6ffb561143f758
gfzum <w48905833@163.com>
2022-08-12 21:13:34 +0800

    update doc

--- xzsBot/bot_plugins/doc.py
+++ xzsBot/bot_plugins/doc.py
@@ -8,5 +8,9 @@ __plugin_usage__ = (
 
 @on_command('doc', aliases=('文档', 'document', '答疑', '在线文档', '在线答疑'))
 async def _(session: CommandSession):
-    await session.send("欢迎查看过往答疑，写下你的问题！\n" + 
-    "https://docs.qq.com/sheet/DZUNXbHRpZEdqSWZH?u=5ee2f3bfee05418ca91d9059048040b0")
+    await session.send("请各位新生在报到前务必填写该报到信息表，在中高风险地区的同学尽快与本班大学长联系。"+
+    "https://docs.qq.com/sheet/DZXR3aVhtcGplVlBz\n\n" +
+    "报到前，除防疫相关要求外，还需要完成12项报到任务，请在完成后填写如下表格：" + 
+    "https://docs.qq.com/sheet/DZWF0eGNwTG9uY2xr\n\n"+
+    "更多问题，请补充在在线答疑文档中：" + 
+    "https://docs.qq.com/sheet/DZUNXbHRpZEdqSWZH\n\n")
--- xzsBot/bot_plugins/misson.py
+++ xzsBot/bot_plugins/misson.py
@@ -8,5 +8,9 @@ __plugin_usage__ = (
 
 @on_command('doc', aliases=('bd', '报到'))
 async def _(session: CommandSession):
-    await session.send("请在8.18前完成报到任务，并填写在表格中喔！\n" + 
-    "https://docs.qq.com/sheet/DZWF0eGNwTG9uY2xr?tab=BB08J2&u=ee5339f8cb074f30b3b95d5c03a70a09")
+    await session.send("请各位新生在报到前务必填写该报到信息表，在中高风险地区的同学尽快与本班大学长联系。"+
+    "https://docs.qq.com/sheet/DZXR3aVhtcGplVlBz\n\n" +
+    "报到前，除防疫相关要求外，还需要完成12项报到任务，请在完成后填写如下表格：" + 
+    "https://docs.qq.com/sheet/DZWF0eGNwTG9uY2xr\n\n"+
+    "更多问题和信息，请补充或查看在线答疑文档：" + 
+    "https://docs.qq.com/sheet/DZUNXbHRpZEdqSWZH\n\n")
